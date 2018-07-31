f881bb37d1f3535b3ea65259581988132f1d7986
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-31 08:39:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,17 @@
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
+
 def begins_with_r(array)
   flag = true
   array.each do |element|
