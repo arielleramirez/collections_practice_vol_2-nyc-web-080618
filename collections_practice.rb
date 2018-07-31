3acbdc1e6f54a23ca9042fe754689e365542193d
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-31 08:35:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,3 +41,15 @@ def find_cool(array)
   end
   container
 end
+
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
